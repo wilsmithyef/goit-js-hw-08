d635d8f36ed20ed8ee1a97cac717f8587fd8e557
wilsmithyef <wilsmithyef@gmail.com>
2023-10-22 13:07:55 -0500

    first changes point 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,43 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+
+import { galleryItems } from "./gallery-items.js";
+// Descrito en la documentación
+import SimpleLightbox from "simplelightbox";
+// Importación adicional de estilos
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionDelay: 250,
+  captionType: "attr",
+  captionsData: "alt",
+});
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const galleryMarkup = galleryItems
+  .map(
+    (item) => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+        <img
+          class="gallery__image"
+          src="${item.preview}"
+        data-source="${item.original}"
+          alt="${item.description}"
+        />
+      </a>
+    </li>
+  `
+  )
+  .join("");
+
+gallery.innerHTML = galleryMarkup;
+
+document.addEventListener("DOMContentLoaded", function () {
+  const gallery = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+});
