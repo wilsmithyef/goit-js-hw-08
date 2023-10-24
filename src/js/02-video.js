88e4fc15d70033ddaf0d1ce71b35cb9634dbd425
wilsmithyef <wilsmithyef@gmail.com>
2023-10-24 03:06:44 -0500

    second point

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.player(iframe);
+
+player.on('play', function () {
+    console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+    console.log('title:', title);
+});
+
+const onPlay = function (data) {
+    const currentTime = data.seconds;
+    localStorage.setItem('videoplayer-current-time', currentTime);
+};
+
+player.on('play', onPlay);
+
+player.off('play');
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') ?? 0);
+
+import throttle from "lodash.throttle";
+
+player.on(
+    'timeupdate',
+    throttle(function (data) {
+        const currentTime = data.seconds;
+        localStorage.setItem('videoplayer-current-time', currentTime);
+    }, 1000)
+);
+
+
 
